7c8b93854f468f8a25c3cfce50985aa4b1bc4fd9
bxrdxs <88906083+bxrdxs@users.noreply.github.com>
2023-02-18 07:52:18 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,17 +3,17 @@ import numpy as np
 #from model import runModel
 from PIL import Image
 #Application heading
-st.title("Dermo DOC")
+st.title("Dermoverse Skin Cancer Detector (V1)")
 
 #Brief summary of what the application does
-st.header("Dermoverse Skin Cancer Detector (V1)")
+st.header("by dermoverse.org)")
 st.subheader("This application can classify potential skin cancer images into two classes, whether they are benign or malignant")
 
 #Information of what kind of images should be uploaded.
-st.subheader("The images uploaded should be a close up of the suspect skin anomoly ")
+st.subheader("The images uploaded should be a close up of the suspect skin anomaly ")
 
 # code, to enable users to upload the file.
-image = st.file_uploader(label = "Users can uplaod their images here. Note multiple files are not accepted and files types are limited to PNG and JPEG", type = ['png', 'jpeg'], accept_multiple_files=False,label_visibility="visible")
+image = st.file_uploader(label = "Users can uplaod their images here. Note multiple files are not accepted and file type is limited to PNG", type = ['png', 'jpeg'], accept_multiple_files=False,label_visibility="visible")
 
 if image is None:
     st.warning("Please Upload a Image")
@@ -23,6 +23,17 @@ else:
     
     st.image(image, caption = image.name, output_format = "auto" )
     modelBtn = st.button("Run Model")
+    
+    
+ finresult = result * 100
+
+    st.write("Dermoverse Console: ", finresult, "%")
+    
+    if result >= 0,5:
+        st.write("Potentially Malignant")
+    else:
+        st.write("Potentially Benign")
+            
 
 #if modelBtn:
     #answer = runModel(image)
