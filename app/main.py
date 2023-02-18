98040c32592d3a23b4f2ffcbb6ec958e21f7fb5b
bxrdxs <88906083+bxrdxs@users.noreply.github.com>
2023-02-18 07:57:58 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,13 @@ import streamlit as st
 import numpy as np
 #from model import runModel
 from PIL import Image
+import numpy as np 
+from tensorflow.keras.models import load_model
+from tesnorflow.keras.preprocessing import image
+from PIL import Image, ImageOps
+from tempfile import NamedTemporaryFile
+import io
+
 #Application heading
 st.title("Dermoverse Skin Cancer Detector (V1)")
 
@@ -23,6 +30,17 @@ else:
     
     st.image(image, caption = image.name, output_format = "auto" )
     modelBtn = st.button("Run Model")
+
+
+def runModel(img):
+    # preprocessing the image
+    img_data = np.ndarray(shape = (1,256,256,3), dtype = np.float64 )
+    img = img.resize((256,256), Image.ANTIALIAS)
+    img_array = np.asarray(img)
+    img_data[0] = (img_array.astype(np.float64) / 255.0)
+
+    model = load_model('melanoma_model4.h5')
+    result = model.predict(img_data)
     
     
  finresult = result * 100
@@ -35,6 +53,5 @@ else:
         st.write("Potentially Benign")
             
 
-#if modelBtn:
-    #answer = runModel(image)
-    #st.success(answer);
+# © Hector Bordas
+# Dermoverse, Inc.
