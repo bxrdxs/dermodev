88a80619d2cca7ee1bb13af3c8c225085577e4e7
bxrdxs <88906083+bxrdxs@users.noreply.github.com>
2023-02-18 08:25:03 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
-#from model import runModel
 from PIL import Image
-import numpy as np 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from PIL import Image, ImageOps
@@ -20,42 +18,38 @@ st.subheader("This application can classify potential skin cancer images into tw
 st.subheader("The images uploaded should be a close up of the suspect skin anomaly ")
 
 # code, to enable users to upload the file.
-image = st.file_uploader(label = "Users can uplaod their images here. Note multiple files are not accepted and file type is limited to PNG", type = ['png', 'jpeg'], accept_multiple_files=False,label_visibility="visible")
-
-result = 0
+image = st.file_uploader(label = "Users can upload their images here. Note multiple files are not accepted and file type is limited to PNG", type = ['png', 'jpeg'], accept_multiple_files=False,label_visibility="visible")
 
 if image is None:
-    st.warning("Please Upload a Image")
+    st.warning("Please Upload an Image")
     st.stop()
+
 else:
-    uploadedImageData = Image.open(image)
+    uploaded_image_data = Image.open(image)
     
     st.image(image, caption = image.name, output_format = "auto" )
-    modelBtn = st.button("Run Model")
+    model_btn = st.button("Run Model")
    
+    def run_model(img):
+        # preprocessing the image
+        img_data = np.ndarray(shape = (1,256,256,3), dtype = np.float64 )
+        img = img.resize((256,256), Image.ANTIALIAS)
+        img_array = np.asarray(img)
+        img_data[0] = (img_array.astype(np.float64) / 255.0)
 
-def runModel(img):
-    # preprocessing the image
-    img_data = np.ndarray(shape = (1,256,256,3), dtype = np.float64 )
-    img = img.resize((256,256), Image.ANTIALIAS)
-    img_array = np.asarray(img)
-    img_data[0] = (img_array.astype(np.float64) / 255.0)
-
-    model = load_model('melanoma_model4.h5')
-    result = model.predict(img_data)
-    finresult = result * 100
-    
-    st.write("Dermoverse Console: ", finresult, "%")
-    
-    if result >= 0.5:
-        st.write("Potentially Malignant")
-    else:
-        st.write("Potentially Benign")
+        model = load_model('melanoma_model4.h5')
+        result = model.predict(img_data)
+        finresult = result[0][0] * 100
+        
+        st.write("Dermoverse Console: {:.2f}%".format(finresult))
+        
+        if result >= 0.5:
+            st.write("Potentially Malignant")
+        else:
+            st.write("Potentially Benign")
+        
+    if model_btn:
+        run_model(uploaded_image_data)
         
         
         
-runModel(image)
-            
-
-# © Hector Bordas
-# Dermoverse, Inc.
